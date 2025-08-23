fe6197d748d8a4e742e5413d955e3f86ca28265f
Stephen McLeish <stephen.mcleish@gmail.com>
2025-08-23 18:03:55 +1000

    fix(types): firebase Timestamp + node types

--- lib/code.ts
+++ lib/code.ts
@@ -1,10 +1,9 @@
 import { Timestamp } from 'firebase/firestore';
 
-export const defaultCodeLength: number = Number.isFinite(
-  Number(process.env.NEXT_PUBLIC_CODE_LENGTH)
-)
-  ? Number(process.env.NEXT_PUBLIC_CODE_LENGTH)
-  : 6;
+export const defaultCodeLength: number =
+  Number.isFinite(Number(process.env.NEXT_PUBLIC_CODE_LENGTH))
+    ? Number(process.env.NEXT_PUBLIC_CODE_LENGTH)
+    : 6;
 
 export function randomCode(len: number = defaultCodeLength): string {
   let s = '';
