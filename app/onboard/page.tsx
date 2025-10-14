58ad99acf6768a76da099d7d4ec979c188143ebb
Stephen McLeish <stephen.mcleish@gmail.com>
2025-10-14 18:03:21 +1000

    fix(onboard): mark page as force-dynamic client

--- app/onboard/page.tsx
+++ app/onboard/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+export const dynamic = "force-dynamic";
 import { FormEvent, useEffect, useMemo, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { getAuth } from "firebase/auth";
